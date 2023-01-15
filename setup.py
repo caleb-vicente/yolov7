71999c935bab31c78be19c1a9c601351be26fcd5
Caleb Vicente Moreno <71934065+caleb-vicente@users.noreply.github.com>
2023-01-16 00:13:00 +0100

    create basic setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='yolov7',
+    version='1.1',
+    packages=['utils', 'utils.aws', 'utils.wandb_logging', 'models'],
+    url='',
+    license='',
+    author='caleb',
+    author_email='',
+    description=''
+)
